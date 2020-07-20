ad23d2dce93b5f1732da8a2d1b0f53f923b53be1
RevYolution <47017138+RevYolution@users.noreply.github.com>
2020-07-20 15:32:59 -0700

    Adding tests

--- RoverTests/UnitTest1.cs
+++ RoverTests/UnitTest1.cs
@@ -32,17 +32,21 @@ namespace RoverTests
             Assert.Equal(10, testRover.Yposition);
         }
 
-        [Fact]
-        public void CanGetSetDirection()
+        [Theory]
+        [InlineData(Direction.N)]
+        [InlineData(Direction.S)]
+        [InlineData(Direction.E)]
+        [InlineData(Direction.W)]
+        public void CanGetSetDirection(Direction testDirection)
         {
             //Arrange
             Rover testRover = new Rover();
 
             //Act
-            testRover.Direction = Direction.N;
+            testRover.Direction = testDirection;
 
             //Assert
-            //Assert.Equal('N', testRover.Direction);
+            Assert.Equal(testDirection, testRover.Direction);
         }
 
         [Fact]
@@ -108,5 +112,54 @@ namespace RoverTests
             //Assert
             Assert.Equal(0, testRover.Xposition);
         }
+
+        [Fact]
+        public void CanMoveRoverFrom_12N_To_13N()
+        {
+            //Arrange
+            Rover testRover = new Rover();
+            int[] testArea = new int[] { 5, 5 };
+            Tuple<int, int, string> testStart = new Tuple<int, int, string>(1, 2, "N");
+            string testCommands = "lmlmlmlmm";
+
+            Rover expectedRover = new Rover();
+            expectedRover.Xposition = 1;
+            expectedRover.Yposition = 3;
+            expectedRover.Direction = Direction.N;
+
+            //Act
+             testRover.RoverMove(testArea, testStart, testCommands);
+
+            //Assert
+            //Assert.Equal<Rover>(expectedRover, testRover);
+            Assert.Equal(1, testRover.Xposition);
+            Assert.Equal(3, testRover.Yposition);
+            Assert.Equal(Direction.N, testRover.Direction);
+        }
+
+        [Fact]
+        public void CanMoveRoverFrom_33E_To_51N()
+        {
+            //Arrange
+            Rover testRover = new Rover();
+            int[] testArea = new int[] { 5, 5 };
+            Tuple<int, int, string> testStart = new Tuple<int, int, string>(3, 3, "E");
+            string testCommands = "mmrmmrmrrm";
+
+            Rover expectedRover = new Rover();
+            expectedRover.Xposition = 5;
+            expectedRover.Yposition = 1;
+            expectedRover.Direction = Direction.E;
+
+            //Act
+            testRover.RoverMove(testArea, testStart, testCommands);
+
+            //Assert
+            //Assert.Equal<Rover>(expectedRover, testRover);
+            Assert.Equal(5, testRover.Xposition);
+            Assert.Equal(1, testRover.Yposition);
+            Assert.Equal(Direction.E, testRover.Direction);
+        }
+
     }
 }
